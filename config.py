8bda7a3ed591bb2cdf14451c320e16c3c48f321a
CjjTom <keralaprime1@gmail.com>
2025-01-06 22:16:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
 START_PIC = os.environ.get("START_PIC","https://envs.sh/ry_.jpg")
-START_MSG = os.environ.get("START_MESSAGE", "ʜᴇʟʟᴏ{mention}ᴇɴᴊᴏʏ sᴇᴄᴜʀᴇ ғɪʟᴇ sᴛᴏʀᴀɢᴇ ɪɴ ᴛʜᴇ ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ ᴇᴀsʏ ᴀᴄᴄᴇss ᴠɪᴀ ᴀ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ"
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 
 .")
 try:
